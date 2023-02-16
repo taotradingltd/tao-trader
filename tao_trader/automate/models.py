ac72522e3d74fd2f5cf427bc9927019de259055c
ClemLanoe <clementlanoe@outlook.com>
2023-02-16 21:17:57 +0000

    Minor changes

--- tao_trader/automate/models.py
+++ tao_trader/automate/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.conf import settings
 from django_countries.fields import CountryField
 from django.utils.translation import gettext_lazy as _
+from tao_utils import tao_security
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # class SiteTypes(models.TextChoices):
 #     bookmaker = "bookmaker", _("bookmaker")
@@ -30,7 +34,7 @@ class Account(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     site = models.ForeignKey(Site, on_delete=models.CASCADE)
     username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
+    password = models.CharField(max_length=200, db_column='password', verbose_name='Password')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -39,3 +43,11 @@ class Account(models.Model):
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name} {self.site.name}"
+    
+    def save(self, *args, **kwargs):
+        self.password = tao_security.encrypt(os.getenv("LADBROKES_PW_KEY"), self.password)
+        super(Account, self).save(*args, **kwargs)
+
+    def decrypted_password(self, obj):
+        self.password = tao_security.decrypt(os.getenv("LADBROKES_PW_KEY"), self.password)
+        return self.password
